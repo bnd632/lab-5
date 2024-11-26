dce29b0bd8ff26e3dd1240b33664ddf650a9a455
Вадим Пулькін <Vadym.Pulkin@cs.khpi.edu.ua>
2024-11-26 20:05:03 +0300

    Добавьте файлы проекта.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,60 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "C:\Users\Вадим\source\repos\lab 2\ConsoleApplication1\ConsoleApplication1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+
+{
+	TEST_CLASS(calcuate_Tests)
+	{
+	public:
+		TEST_METHOD(calculate_1) {
+			int n = 4;
+			double x_1 = 1.0;
+			double x_2 = 2.0;
+			double step = 2;
+			double expected = 6;
+
+			double actual = Calculate(x_1, x_2, step, n);
+			Assert::AreEqual(expected, actual);
+
+		};
+	public:
+		TEST_METHOD(calculate_2) {
+			int n = 4;
+			double x_1 = 2.0;
+			double x_2 = 2.0;
+			double step = 3;
+			double expected = 12;
+
+			double actual = Calculate(x_1, x_2, step, n);
+			Assert::AreEqual(expected, actual);
+
+
+
+		};
+
+	};
+};
+TEST_CLASS(checkValidParams_Tests)
+{
+public:
+
+	TEST_METHOD(checkValidParams_get10_exceptionNotThrown)
+	{
+		int n = 10;
+		try
+		{
+			checkValidParams(n);
+			Assert::IsTrue(true);
+		}
+		catch (...)
+		{
+			Assert::Fail();
+
+		}
+
+	}
+};
---
+++ lab5 ipz/lab5 ipz.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+void checkValidInput() {
+    if (cin.fail()) {
+        cin.clear();
+        cin.ignore(10000, '\n');
+        throw "Invalid input. Please enter a number.";
+    }
+}
+
+void checkValidParams(int n) {
+    if (n <= 3) {
+        throw "Invalid value for n. n must be greater than 3.";
+    }
+}
+
+double Calculate(double x_1, double x_2, double step, int n) {
+    double total_y = 0.0;
+
+    for (double current_x = x_1; (step > 0 ? current_x <= x_2 : current_x >= x_2); current_x += step) {
+        cout << "Current value of x: " << current_x << endl;
+        double y = 1.0;
+
+        if (current_x < 3) {
+            for (int i = 1; i <= n - 2; i++) {
+                y *= (i + current_x);
+            }
+        }
+        else {
+            for (int i = 1; i <= n; i++) {
+                double sum = 0.0;
+                for (int j = 1; j <= n + 3; j++) {
+                    sum += (i + 1.0 / (4.0 * j * j));
+                }
+                y *= sum;
+            }
+        }
+
+        cout << "Result y = " << y << endl;
+        total_y += y;
+    }
+
+    return total_y;
+}
+
+int main() {
+    double x_1, x_2, step;
+    int n;
+    char restart;
+
+    do {
+        while (true) {
+            try {
+                cout << "Enter x_1: ";
+                cin >> x_1;
+                checkValidInput();
+
+                cout << "Enter x_2 (x_2 >= x_1): ";
+                cin >> x_2;
+                checkValidInput();
+                if (x_2 < x_1) {
+                    throw "Invalid input. x_2 must be greater than or equal to x_1.";
+                }
+
+                cout << "Enter step: ";
+                cin >> step;
+                checkValidInput();
+                if ((x_1 < x_2 && step <= 0) || (x_1 > x_2 && step >= 0)) {
+                    throw "Invalid input. Step must move x_1 towards x_2.";
+                }
+
+                while (true) {
+                    try {
+                        cout << "Enter n (n > 3) must be an integer: ";
+                        cin >> n;
+                        checkValidInput();
+                        checkValidParams(n);
+                        break;
+                    }
+                    catch (const char* ex) {
+                        cout << ex << endl;
+                    }
+                }
+
+                double result = Calculate(x_1, x_2, step, n);
+                cout << "Total result of all y values: " << result << endl;
+                break;
+            }
+            catch (const char* ex) {
+                cout << ex << endl;
+            }
+        }
+
+        cout << "Do you want to restart the program? (y/n): ";
+        cin >> restart;
+        cin.ignore(10000, '\n');
+
+    } while (restart == 'y' || restart == 'Y');
+
+    cout << "Program terminated." << endl;
+    return 0;
+}
